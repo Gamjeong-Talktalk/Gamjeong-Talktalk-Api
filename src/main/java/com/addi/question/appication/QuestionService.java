096591983122647cde402a5237627204d67d4827
kimyechan <tioon74@naver.com>
2024-02-07 19:21:29 +0900

    style : 질문 import문 수정

--- src/main/java/com/addi/question/appication/QuestionService.java
+++ src/main/java/com/addi/question/appication/QuestionService.java
@@ -1,19 +1,11 @@
 package com.addi.question.appication;
 
-import com.addi.global.exception.BusinessException;
 import com.addi.question.appication.dto.QuestionResponse;
 import com.addi.question.domain.Questions;
-import com.addi.stt.exception.VoiceError;
-import com.google.cloud.speech.v1.*;
-import com.google.protobuf.ByteString;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
--- src/main/java/com/addi/question/presentation/QuestionController.java
+++ src/main/java/com/addi/question/presentation/QuestionController.java
@@ -2,16 +2,11 @@ package com.addi.question.presentation;
 
 import com.addi.question.appication.QuestionService;
 import com.addi.question.appication.dto.QuestionResponse;
-import com.addi.stt.appication.VoiceService;
-import com.addi.translation.application.PapagoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Slf4j
 @RestController
