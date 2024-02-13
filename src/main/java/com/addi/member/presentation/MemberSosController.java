dd144baa486839e9b2b7411cc67fa43a14d3c37e
kimyechan <tioon74@naver.com>
2024-02-13 16:24:06 +0900

    refactor : SOS 컨트롤러 분리

--- src/main/java/com/addi/member/presentation/MemberSosController.java
+++ src/main/java/com/addi/member/presentation/MemberSosController.java
@@ -14,47 +14,47 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class MemberSosController {
 
-	private final MemberService memberService;
+    private final MemberService memberService;
 
-	// SOS 요청 송신
-	@GetMapping("/busan/sasang/sos/{memberId}")
-	public ResponseEntity<LoginToUserResponse> sendSOSRequest(@PathVariable Long memberId){
-		memberService.sendRequest(memberId);
+    // SOS 요청 송신
+    @GetMapping("/busan/sasang/sos/{memberId}")
+    public ResponseEntity<LoginToUserResponse> sendSOSRequest(@PathVariable Long memberId){
+        memberService.sendRequest(memberId);
 
-		return ResponseEntity.ok().build();
-	}
+        return ResponseEntity.ok().build();
+    }
 
-	//가사지원
-	@GetMapping("/busan/sasang/chores/{memberId}")
-	public ResponseEntity<LoginToUserResponse> sendChoreRequest(@PathVariable Long memberId){
-		memberService.sendRequest(memberId);
+    //가사지원
+    @GetMapping("/busan/sasang/chores/{memberId}")
+    public ResponseEntity<LoginToUserResponse> sendChoreRequest(@PathVariable Long memberId){
+        memberService.sendRequest(memberId);
 
-		return ResponseEntity.ok().build();
-	}
+        return ResponseEntity.ok().build();
+    }
 
-	//외출동행
-	@GetMapping("/busan/sasang/outside/{memberId}")
-	public ResponseEntity<LoginToUserResponse> sendOutsideRequest(@PathVariable Long memberId){
-		memberService.sendRequest(memberId);
+    //외출동행
+    @GetMapping("/busan/sasang/outside/{memberId}")
+    public ResponseEntity<LoginToUserResponse> sendOutsideRequest(@PathVariable Long memberId){
+        memberService.sendRequest(memberId);
 
-		return ResponseEntity.ok().build();
-	}
+        return ResponseEntity.ok().build();
+    }
 
-	//말벗
-	@GetMapping("/busan/sasang/friend/{memberId}")
-	public ResponseEntity<LoginToUserResponse> sendFriendRequest(@PathVariable Long memberId){
-		memberService.sendRequest(memberId);
+    //말벗
+    @GetMapping("/busan/sasang/friend/{memberId}")
+    public ResponseEntity<LoginToUserResponse> sendFriendRequest(@PathVariable Long memberId){
+        memberService.sendRequest(memberId);
 
-		return ResponseEntity.ok().build();
-	}
+        return ResponseEntity.ok().build();
+    }
 
-	//문화 활동 신청
-	@GetMapping("/busan/sasang/culture/{memberId}")
-	public ResponseEntity<LoginToUserResponse> sendCultureRequest(@PathVariable Long memberId){
-		memberService.sendRequest(memberId);
+    //문화 활동 신청
+    @GetMapping("/busan/sasang/culture/{memberId}")
+    public ResponseEntity<LoginToUserResponse> sendCultureRequest(@PathVariable Long memberId){
+        memberService.sendRequest(memberId);
 
-		return ResponseEntity.ok().build();
-	}
+        return ResponseEntity.ok().build();
+    }
 
 
 }
