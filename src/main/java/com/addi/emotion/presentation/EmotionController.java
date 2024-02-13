9d833f902dad2ffb010dc07630d3f925de5c641b
kimyechan <tioon74@naver.com>
2024-02-14 02:56:33 +0900

    feat : 감정 분석 타임리프 기능 추가

--- src/main/java/com/addi/emotion/presentation/EmotionController.java
+++ src/main/java/com/addi/emotion/presentation/EmotionController.java
@@ -7,8 +7,11 @@ import com.addi.emotion.dto.TodayEmotionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDate;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,18 +21,32 @@ public class EmotionController {
 
     private final ConversationRepository conversationRepository;
 
-    @GetMapping("/busan/sasang/today/{memberId}")
+    /*@GetMapping("/busan/sasang/today/{memberId}")
     public ResponseEntity<TodayEmotionResponse> getEmotionToday(@PathVariable Long memberId){
         TodayEmotionResponse todayEmotionResponse = emotionService.getEmotionToday(memberId);
 
         return ResponseEntity.ok(todayEmotionResponse);
-    }
+    }*/
 
     @GetMapping("/busan/sasang/week/{memberId}")
-    public ResponseEntity<OneWeekEmotionResponse> getEmotionOneWeek(@PathVariable Long memberId){
+    public String getEmotionOneWeek(Model model, @PathVariable Long memberId){
         OneWeekEmotionResponse oneWeekEmotionResponse = emotionService.getEmotionOneWeek(memberId);
+        model.addAttribute("emotionResponse", oneWeekEmotionResponse);
+        model.addAttribute("emotionDateNow", LocalDate.now().minusDays(1).toString());
+        model.addAttribute("emotionDatePast",LocalDate.now().minusDays(8).toString());
 
-        return ResponseEntity.ok(oneWeekEmotionResponse);
+
+        return "emotion/weekEmotion";
+
+    }
+
+    @GetMapping("/busan/sasang/today/{memberId}")
+    public String showEmotion(Model model, @PathVariable Long memberId) {
+        TodayEmotionResponse todayEmotionResponse = emotionService.getEmotionToday(memberId);
+        model.addAttribute("emotionResponse", todayEmotionResponse);
+        model.addAttribute("emotionDate", LocalDate.now().minusDays(1).toString());
+
+        return "emotion/todayEmotion"; // 이 부분에서는 위에서 작성한 Thymeleaf HTML 파일의 이름을 반환합니다.
     }
 
 
